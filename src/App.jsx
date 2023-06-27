505b12b212aba2a123f391fb85d6f5e8929f5f10
Leonardo Galindo <leonardogalindo8@gmail.com>
2023-06-27 20:12:02 -0300

    Inicio do projeto

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import './App.css'
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import {v4 as uuidv4} from 'uuid'
+
+const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar progamação',
+      completed: false
+    },
+    {
+      id: '2',
+      title: 'Ler livros',
+      completed: true
+    }
+  ]);
+
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map(task => {
+      if(task.id === taskId) return {...task, completed: !task.completed}
+      
+      return task
+    })
+
+    setTasks(newTasks)
+  }
+
+  const handleTaskAddition = (taskTitle) => {
+    const newTasks = [...tasks, {
+      title: taskTitle,
+      id: uuidv4(),
+      completed: false,
+    }]
+
+    setTasks(newTasks)
+  }
+
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = tasks.filter(task => task.id !== taskId)
+    setTasks(newTasks)
+  }
+
+  return (
+    <div>
+      <div className="container">
+        <AddTask handleTaskAddition = {handleTaskAddition} handleTaskClick />
+        <Tasks tasks = {tasks} handleTaskClick ={handleTaskClick} handleTaskDeletion ={handleTaskDeletion}/>
+      </div>
+
+    </div>
+  );
+};
+
+export default App;
